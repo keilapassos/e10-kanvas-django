4fc0f4d65e9ccdb3c750f59b83553b1be9998210
Keila Aparecida Rodrigues Passos <keila.rodrigues.passos@gmail.com>
2022-03-21 01:55:24 -0300

    courses patch and delete. New report-txt

--- courses/views.py
+++ courses/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from .models import Course
 from .serializers import CourseSerializer
 from rest_framework.authentication import authenticate, TokenAuthentication
-from courses.permissions import CoursePermission
+from courses.permissions import CoursePermission, CoursesByIdPermission
 
 
 # Create your views here.
@@ -28,7 +28,7 @@ class CourseView(APIView):
 
     course = Course.objects.create(
       name = request.data['name'],
-      demo_time = request.data['demo_time'],
+      demo_time = request.data['demo_time']+':00',
       created_at = datetime.now(),
       link_repo = request.data['link_repo']
     )
@@ -48,7 +48,7 @@ class CourseView(APIView):
 class CourseByIdView(APIView):
 
   authentication_classes = [TokenAuthentication]
-  permission_classes = [CoursePermission]  
+  permission_classes = [CoursesByIdPermission]  
 
   def get(self, request, course_id=''):
 
@@ -63,4 +63,44 @@ class CourseByIdView(APIView):
     serializer = CourseSerializer(course)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+  def patch(self, request, course_id=''):
+    
+    try:
+      course = Course.objects.get(uuid=course_id)
+
+      serialized = CourseSerializer(data=request.data, partial=True)
+      serialized.is_valid()
+      
+      data = {**serialized.validated_data}
+
+      if 'name' in data:
+        if Course.objects.filter(name=request.data['name']).exists() == True:
+          response = {"message": "This course name already exists"}
+          return Response(response, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+      for key, value in data.items():
+        # course[key] = value
+        course.__dict__[key] = value
+      
+      course.save()
+
+      course = Course.objects.get(uuid=course_id)
+      serialized = CourseSerializer(course)
+
+      return Response(serialized.data)
+
+    except Course.DoesNotExist:
+      return Response(
+        {'message': 'Course does not exist'}, status=status.HTTP_404_NOT_FOUND,
+      )
+
+  def delete(self, request, course_id=''):
+
+    try:
+      course = Course.objects.get(uuid=course_id).delete()
+
+      return Response(status=status.HTTP_204_NO_CONTENT)
+    except Course.DoesNotExist:
+      return Response({"message": "Course does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
